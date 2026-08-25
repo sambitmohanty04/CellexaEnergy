b8a0a8b8c286b6acf78edefb0fb26ec847510b1e
Sam <sammohanty04@gmail.com>
2026-08-25 21:04:07 +0530

    Prepare frontend for deployment

--- client/src/service/api.tsx
+++ client/src/service/api.tsx
@@ -1,7 +1,18 @@
+// import axios from "axios";
+
+// const API_URL = axios.create({
+//   baseURL: "http://localhost:5001/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default API_URL;
+
 import axios from "axios";
 
 const API_URL = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
