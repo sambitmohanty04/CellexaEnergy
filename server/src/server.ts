a2b0228b6165d6de0847708429d764c26024ddc8
Sam <sammohanty04@gmail.com>
2026-08-27 11:59:46 +0530

    server.js file updated

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,14 +4,6 @@ dotenv.config();
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
-const PORT = process.env.PORT || 5001;
+await connectDB();
 
-const startServer = async (): Promise<void> => {
-  await connectDB();
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-};
-
-startServer();
+export default app;
